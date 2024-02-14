06391dfcddf5e8d6f22b78fe3774e40df5222bf5
farukk98 <kadicfaruk7@gmail.com>
2024-02-14 20:02:27 +0100

    modifided the list

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -11,14 +11,12 @@ const List = () => {
         console.log(data.searchInput)
     }
     return ( 
-        <div className=" bg-blue-300 border rounded-xl h-[300px] w-[300px] m-12 px-8 flex flex-col p-2">
-            <div className="flex mr-4">
-                <form onSubmit={handleSubmit(onSubmit)}>
+        <div className=" bg-blue-300 border rounded-xl flex flex-col w-[300px] p-3 ml-[100px] mt-7">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
                     <textarea name="id" {...register("searchInput")} className="m-4 h-8 resize-none"/>
                     <textarea name="id" {...register("searchInput")} className="m-4 h-20 resize-none"/ >
                     <textarea name="id" {...register("searchInput")} className="m-4 h-10 resize-none"/ >
                 </form>
-            </div>
         </div> 
      );
 }
